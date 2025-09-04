0d60e248d8c2f7c4c2ef1ab3569a8953005af65e
Felber Colin <colin.felber@stud.hslu.ch>
2025-09-04 21:24:08 +0200

    rename

--- obj/Debug/net9.0/LeetCode.AssemblyInfo.cs
+++ obj/Debug/net9.0/LeetCode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc3280f02f923cb2fb91e56cdaa18d424c6e43c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d982761dbf874b3b95ae7d17df5e482934556f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
