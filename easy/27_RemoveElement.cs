4f7e3a8d44c3edd6b4c693f8547014fc29e26e72
Felber Colin <colin.felber@stud.hslu.ch>
2025-09-06 23:11:50 +0200

    Add Remove Element

---
+++ easy/27_RemoveElement.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.easy;
+
+public class RemoveElemente {
+    public int RemoveElement(int[] nums, int val) 
+    {
+        if (nums.Length == 0) return 0;
+        if (val == null) return 0;
+        
+        int counter = 0;
+        int nextPosition = 0;
+        
+        for (int i = 0; i < nums.Length; i++) {
+            
+            int number = nums[i];
+            
+            if (number != val) {
+                nums[nextPosition] = nums[i];
+                nextPosition++;
+                counter++;
+            }    
+        }
+        return counter;
+    }
+}
