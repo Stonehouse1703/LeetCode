8d6331a32863af8269a7264c8c1b36d356aedc42
Felber Colin <colin.felber@stud.hslu.ch>
2025-09-09 08:43:29 +0200

    add 58. Length Of Last Word

---
+++ easy/58_LengthOfLastWord.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.easy;
+
+/*
+ Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal substring consisting of non-space characters only.
+
+Example 1:
+
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+Example 2:
+
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+Example 3:
+
+Input: s = "luffy is still joyboy"
+Output: 6
+Explanation: The last word is "joyboy" with length 6.
+ */
+
+public class LengthOfLastWord {
+      public int LengthLastWord(string s) {
+          var words = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+          if (words.Length == 0) return 0;
+          return words[^1].Length;
+      }
+}
